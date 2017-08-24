40613cc25540a58e5e5b9ea12253d6fcaa5a669e
ripee <ripee@rogers.com>
2017-08-24 19:47:22 +0200

    Update trltevzw-vendor-blobs.mk

--- trltevzw/trltevzw-vendor-blobs.mk
+++ trltevzw/trltevzw-vendor-blobs.mk
@@ -59,7 +59,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/trltevzw/proprietary/lib/librmnetctl.so:system/lib/librmnetctl.so \
     vendor/samsung/trltevzw/proprietary/lib/libsec-ril.so:system/lib/libsec-ril.so \
     vendor/samsung/trltevzw/proprietary/lib/libsecnativefeature.so:system/lib/libsecnativefeature.so \
-    vendor/samsung/trltevzw/proprietary/lib/libsecril-client-sap.so:system/lib/libsecril-client-sap.so \
     vendor/samsung/trltevzw/proprietary/vendor/lib/libconfigdb.so:system/vendor/lib/libconfigdb.so \
     vendor/samsung/trltevzw/proprietary/vendor/lib/libmdmdetect.so:system/vendor/lib/libmdmdetect.so \
     vendor/samsung/trltevzw/proprietary/vendor/lib/libril-qcril-hook-oem.so:system/vendor/lib/libril-qcril-hook-oem.so \
